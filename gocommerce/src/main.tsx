7753283cebe673f99227e27f0d8f585cfd0de675
dnoulet <daniel.noulet@gmail.com>
2026-08-19 20:57:45 +0200

    chore: remove strict mode
    removing strcit so thaa the frontend only calls the back end onc

--- gocommerce/src/main.tsx
+++ gocommerce/src/main.tsx
@@ -1,16 +1,13 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import './App.css'
 import App from './App.tsx'
-import {BrowserRouter} from "react-router-dom"
-import {GlobalStateProvider} from "./state/global-state.tsx";
+import { BrowserRouter } from "react-router-dom"
+import { GlobalStateProvider } from "./state/global-state.tsx";
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <BrowserRouter>
-            <GlobalStateProvider>
-                <App/>
-            </GlobalStateProvider>
-        </BrowserRouter>
-    </StrictMode>,
+  <BrowserRouter>
+    <GlobalStateProvider>
+      <App />
+    </GlobalStateProvider>
+  </BrowserRouter>,
 )
